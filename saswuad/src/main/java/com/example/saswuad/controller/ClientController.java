de9e476aebea2fe75c565cb724bd59bb5edea3b9
Jhenrique44 <70330647+Jhenrique44@users.noreply.github.com>
2021-03-23 20:15:47 -0300

    pagina

--- saswuad/src/main/java/com/example/saswuad/controller/ClientController.java
+++ saswuad/src/main/java/com/example/saswuad/controller/ClientController.java
@@ -11,6 +11,9 @@ import com.example.saswuad.dto.ClientUpdateDTO;
 import com.example.saswuad.services.ClientService;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageRequest;
+import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -19,6 +22,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
@@ -30,8 +34,16 @@ public class ClientController {
     private ClientService service;
 
     @GetMapping
-    public ResponseEntity<List<ClientDTO>> getClients(){
-        List <ClientDTO> list = service.getClients();
+    public ResponseEntity<Page<ClientDTO>> getClients(
+        @RequestParam(value = "page", defaultValue = "0") Integer page,
+        @RequestParam(value = "linesPerPage", defaultValue = "6") Integer linesPerPage,
+        @RequestParam(value = "direction", defaultValue = "ASC") String direction,
+        @RequestParam(value = "orderBy", defaultValue = "id") String orderBy
+
+    ){
+        PageRequest pageRequest = PageRequest.of(page, linesPerPage, Direction.valueOf(direction),orderBy);
+
+        Page <ClientDTO> list = service.getClients(pageRequest);
         return ResponseEntity.ok().body(list);
     }
 
--- saswuad/src/main/java/com/example/saswuad/repository/ClientRepository.java
+++ saswuad/src/main/java/com/example/saswuad/repository/ClientRepository.java
@@ -2,11 +2,20 @@ package com.example.saswuad.repository;
 
 import com.example.saswuad.entities.Client;
 
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 
 @Repository
 public interface ClientRepository extends JpaRepository <Client,Long> {
     
+
+    //Uma consulta JPQL (sintaxe diferente de SQL)!!!
+    @Query("SELECT c FROM Client c") //SELECT da entidade Client e não da tabela
+    public Page<Client> find(Pageable pageRequest);
+
+
 }
